e3f5446c27fff4caec7924a9da5946b01849d5e6
Rosco Kalis <roscokalis@gmail.com>
2020-05-02 19:47:17 +0200

    Add start of liquidations tests

---
+++ sharktokens/test/SharkToken.Liquidations.test.js
@@ -0,0 +1,44 @@
+/* global web3, artifacts, contract, before */
+const SharkToken = artifacts.require('SharkToken')
+const ILendingPool = artifacts.require('ILendingPool')
+const IERC20 = artifacts.require('IERC20')
+const ILendingPoolAddressesProvider = artifacts.require('ILendingPoolAddressesProvider')
+const chai = require("chai")
+const truffleAssert = require('truffle-assertions')
+const { setup } = require('./fixture/dummy_loans')
+const { ADDRESSES_PROVIDER, DAI_ADDRESS, BORROWER, USDC_ADDRESS } = require('./fixture/addresses')
+const { replacePriceOracle } = require('./fixture/dummy_loans')
+
+chai.use(require('chai-bn')(web3.utils.BN));
+const { expect } = chai
+
+contract('SharkToken', (accounts) => {
+  const admin = accounts[0]
+  const investor = BORROWER
+  const target = BORROWER
+
+  let sharkToken
+  let daiToken
+
+  beforeEach(async () => {
+    sharkToken = await SharkToken.new('Shark Dai', 'shDAI', DAI_ADDRESS)
+    daiToken = await IERC20.at(DAI_ADDRESS)
+    await replacePriceOracle()
+    await daiToken.approve(sharkToken.address, '-1', { from: investor })
+    await sharkToken.deposit(web3.utils.toBN(web3.utils.toWei('100')), { from: investor })
+  })
+
+  describe('LiquidateOnAave', () => {
+    it('can liquidate', async () => {
+      // given
+      const amount = web3.utils.toBN(web3.utils.toWei('20'))
+
+      // when
+      // Currently reverts, because USDC is not converted to DAI in the contract
+      const tx = await sharkToken.liquidateOnAave(USDC_ADDRESS, target, amount)
+
+      // then
+      // Do checks
+    })
+  })
+})

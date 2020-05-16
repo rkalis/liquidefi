569a681a8e78f034c373b46c7de5fa682e21b404
snaketh4xor <snaaketh4xor@protonmail.com>
2020-05-16 22:07:01 +0530

    Updates dydx v2

--- sharktokens/contracts/dydx/DyDx.sol
+++ sharktokens/contracts/dydx/DyDx.sol
@@ -5,6 +5,4 @@ import "./Structs.sol";
 
 contract DyDx {
     function operate(Structs.Info[] memory accounts, Structs.ActionArgs[] memory actions) public;
-    function getMarketIsClosing(uint256 marketId) public view returns (bool);
-    function getMarketTokenAddress(uint256 marketId) public view returns (address);
 }
--- sharktokens/contracts/dydx/DyDxBorrow.sol
+++ sharktokens/contracts/dydx/DyDxBorrow.sol
@@ -1,13 +1,40 @@
 pragma solidity ^0.6.0;
 
 import "./DyDx.sol";
+import "./Structs.sol";
+import "./lib/dappsys/DSAuth.sol";
 
 contract DyDxBorrow {
 
-function borrow() {
-  
+address internal soloMarginAddress;
+mapping(address => uint256) internal tokenAddressToMarketId;
+
+constructor(address _soloMarginAddress) public {
+  soloMarginAddress = _soloMarginAddress;
 }
 
+function registerMarketID((uint256 marketId,address _tokenaddress) external auth {
+  tokenAddressToMarketId[_tokenAddress] = marketId;
+}
+
+function borrow(address tokenAddress, uint256 tokenAmount) external {
+  Structs.Info[] memory infos = new Structs.Info[](1);
+  Structs.ActionArgs[] memory args = new Structs.ActionArgs[](3);
+  infos[0] = Structs.Info(address(this), 0);
+
+  ISoloMargin solo = ISoloMargin(_soloMarginAddress);
+  Strcuts.ActionArgs[] memory operations = new Structs.ActionArgs[](3);
+
+
+
+
+
+
+
+
+
+
+}
 
 
 
